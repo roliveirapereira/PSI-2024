c21333e4dc18587f7b5e1abd9ba497617c6e1c22
Rodrigo <roliveirapereira.dev@gmail.com>
2025-02-08 15:56:47 -0300

    vercel

--- lojaAdmin/wsgi.py
+++ lojaAdmin/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lojaAdmin.settings.development')  # Aqui
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lojaAdmin.settings')
+
+app = get_wsgi_application() 
+
